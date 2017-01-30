ef5958a8de0bced7d1da486db6386f3e67c1f5cd
Pat Holland <email": null>
2017-01-30 10:40:47 -0800

    add letter grade logic

--- part-1/student.js
+++ part-1/student.js
@@ -11,3 +11,21 @@ Student.prototype.averageScore = function(){
   })
   return Math.floor(total/arrayLength)
 }
+
+Student.prototype.letterGrade = function(){
+  if (this.averageScore() >= 90){
+    return "A"
+  }
+  else if (this.averageScore() >=80){
+    return "B"
+  }
+ else if (this.averageScore() >=70){
+    return "C"
+  }
+   else if (this.averageScore() >=60){
+    return "D"
+  }
+   else {
+    return "F"
+  }
+}
